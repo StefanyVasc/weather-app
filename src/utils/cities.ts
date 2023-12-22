1d1dfa3bb4158c65862d85a34ab6b0f0534d7571
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-22 02:19:26 -0300

    refactor: cities util

--- src/utils/cities.ts
+++ src/utils/cities.ts
@@ -7,7 +7,7 @@ export interface City {
 export const  cities: City[] = [
   {id: 1, city: 'Dallol', location: 'NG'},
   {id: 2,city: 'Fairbanks', location: 'US'},
-  {id: 3, city: 'Londres', location: 'GB'},
+  {id: 3, city: 'London', location: 'GB'},
   {id: 4, city: 'Recife', location: 'BR'},
   {id: 5, city: 'Vancouver', location: 'CA'},
   {id: 6, city: 'Yakutsk', location: 'RU'},
