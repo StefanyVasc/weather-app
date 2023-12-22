3c11c618670de7cdcacac79d9d38eaefe8eecde3
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-21 22:54:53 -0300

    feat: create not found page

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,11 @@
+
+export function NotFound() {
+  return (
+    <div>
+      <h2>404 - Not Found</h2>
+      <p>Sorry, the page you are looking for might be in another castle.</p>
+    </div>
+  );
+}
+
+
