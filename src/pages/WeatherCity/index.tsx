519e1d2a1b67416bad3b2efd78497783d041368a
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-22 02:27:49 -0300

    feat: create weather utils and page WeatherCity

--- src/pages/WeatherCity/index.tsx
+++ src/pages/WeatherCity/index.tsx
@@ -1,20 +1,93 @@
-import { Header } from "../../components/Header";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Header } from '../../components/Header';
+import { format } from 'date-fns';
+import {
+  getTemperatureForTime,
+  formatWeatherData,
+  HourData,
+  getCurrentHourCondition,
+} from '../../utils/weatherUtils';
 
 interface WeatherCityProps {
   cityName: string;
-  temperature?: string;
-  description?: string;
-  location: string;
 }
 
-export function WeatherCity({ cityName, temperature, description, location }: WeatherCityProps) {
+interface WeatherData {
+  hour: HourData[];
+  astro?: {
+    sunrise: string;
+    sunset: string;
+  };
+}
+
+const WEATHER_API_URL = 'http://api.weatherapi.com/v1/forecast.json';
+const API_KEY = '4f4e2ab84aa84297a37221347232112';
+
+export function WeatherCity({ cityName }: WeatherCityProps) {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        setLoading(true);
+
+        if (!API_KEY) throw new Error('A chave de API não está definida.');
+
+        const today = new Date();
+        const formattedDate = format(today, 'yyyy-MM-dd');
+        const apiUrl = `${WEATHER_API_URL}?key=${API_KEY}&q=${cityName}&dt=${formattedDate}&hour=3,9,15,21`;
+
+        const response = await axios.get(apiUrl);
+
+        if (!response?.data || response?.status !== 200) {
+          throw new Error('Erro ao buscar dados meteorológicos');
+        }
+
+        setWeatherData(response.data.forecast.forecastday[0]);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        console.error(`Erro ao buscar dados meteorológicos para ${cityName}: ${error.message}`);
+      
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeatherData();
+  }, [cityName]);
+
+  const currentHourCondition = getCurrentHourCondition(weatherData?.hour);
+  const schedules = [3, 9, 15, 21];
+
   return (
     <div>
-      <Header />
+      <Header selectedCity={cityName} />
       <h1>{cityName}</h1>
-      <p>Temperature: {temperature}</p>
-      <p>Description: {description}</p>
-      <p>Location: {location}</p>
+
+      {loading && <p>Carregando...</p>}
+
+      {weatherData && (
+        <div>
+          <p>Condição atual: {formatWeatherData(currentHourCondition).condition}</p>
+          <p>Temperature Now: {formatWeatherData(currentHourCondition).temperature}</p>
+
+          {schedules.map((hour) => {
+            const hourData = getTemperatureForTime(weatherData.hour, hour);
+            return (
+              <p key={hour}>
+                Temperature {formatWeatherData(hourData)?.temperature} {formatWeatherData(hourData)?.period}
+              </p>
+            );
+          })}
+          <p>Wind Speed: {formatWeatherData(currentHourCondition).windSpeed}</p>
+          <p>Humidity: {formatWeatherData(currentHourCondition).humidity}</p>
+          <p>Sunrise: {weatherData.astro?.sunrise}</p>
+          <p>Sunset: {weatherData.astro?.sunset}</p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/utils/weatherUtils.ts
@@ -0,0 +1,56 @@
+export interface HourData {
+  temp_c: number;
+  time: string;
+  condition: {
+    text: string;
+    icon: string;
+    code: number;
+  };
+  wind_kph: number;
+  humidity: number;
+}
+
+
+function formatTemperature(temperature: number | string): string  {
+  return `${temperature}°C`;
+}
+
+function formatWindSpeed(windSpeed: number | undefined): string {
+  return windSpeed !== undefined ? `${windSpeed} km/h` : 'N/A';
+}
+
+function formatHumidity(humidity: number | undefined): string {
+  return humidity !== undefined ? `${humidity}%` : 'N/A';
+}
+
+function getPeriod(hourData: HourData | undefined): string {
+  const hourOfDay = new Date(hourData?.time || 0).getHours();
+
+  if (hourOfDay >= 0 && hourOfDay < 6) return 'Dawn';
+  if (hourOfDay < 12) return 'Morning';
+  if (hourOfDay < 18) return 'Afternoon';
+  
+  return 'Night';
+}
+
+
+export function getCurrentHourCondition(hours: HourData[] | undefined) {
+  const currentHour = new Date().getHours();
+  return hours?.find((hour) => new Date(hour.time).getHours() === currentHour);
+}
+
+export function getTemperatureForTime(hours: HourData[], targetHour: number): HourData | undefined {
+  return hours.find((hour) => new Date(hour.time).getHours() === targetHour);
+}
+
+export function formatWeatherData(hourData: HourData | undefined) {
+  return {
+    condition: hourData?.condition.text || 'N/A',
+    temperature: formatTemperature(hourData?.temp_c || 'N/A'),
+    windSpeed: formatWindSpeed(hourData?.wind_kph),
+    humidity: formatHumidity(hourData?.humidity),
+    period: getPeriod(hourData),
+  };
+}
+
+
