04dce781211d1cd9ece767f8c42042bb8c7417aa
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-21 22:57:39 -0300

    feat: create functional page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,31 @@
+import { GlobeHemisphereEast } from '@phosphor-icons/react';
+import { NavLink } from 'react-router-dom';
+import './style.scss';
+import { cities, City } from '../../utils/cities';
+
 export function Home() {
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home__container">
+      <header className='home__header'>
+        <span>Weather</span>
+        <p>select a city</p>
+      </header>
+      <main className='home__main'>
+        <GlobeHemisphereEast size={140} weight='thin'/>
+
+        <div className="cities__wrapper">
+          {cities.map((item: City) => (
+            <NavLink
+              key={item.id}
+              to={`/${item.city.toLowerCase()}`}
+              className="custom-link"
+            >
+              {item.city}
+            </NavLink>
+          ))}
+        </div>
+      </main>
     </div>
-  )
+  );
 }
