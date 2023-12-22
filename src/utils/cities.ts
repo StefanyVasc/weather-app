f892d824bf642312facb7f17b46d6cb2b2dd1d78
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-21 22:55:48 -0300

    feat: create utils for cities

---
+++ src/utils/cities.ts
@@ -0,0 +1,14 @@
+export interface City {
+  id: number;
+  city: string;
+  location: string; 
+}
+
+export const  cities: City[] = [
+  {id: 1, city: 'Dallol', location: 'NG'},
+  {id: 2,city: 'Fairbanks', location: 'US'},
+  {id: 3, city: 'Londres', location: 'GB'},
+  {id: 4, city: 'Recife', location: 'BR'},
+  {id: 5, city: 'Vancouver', location: 'CA'},
+  {id: 6, city: 'Yakutsk', location: 'RU'},
+]
