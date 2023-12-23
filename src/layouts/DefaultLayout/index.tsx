050108394fbc4a45881828afb42eecae6e3e34b4
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-22 22:45:55 -0300

    refactor: update DefaultLayout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet} from 'react-router-dom'
 import './style.scss'
 
 export function DefaultLayout() {
   return (
-    <div className='layout-container'>
-      <Outlet />
-    </div>
-  )
+    <div className="layout-container">
+    <Outlet />
+  </div>
+);
 }
+
