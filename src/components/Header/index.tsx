14a07d2e381a74ec067d6e2a18bffa0b96663bae
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-23 17:44:31 -0300

    refactor: update Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,9 @@ import { ArrowLeft } from '@phosphor-icons/react'
 import { NavLink } from 'react-router-dom'
 import './style.scss'
 
-interface HeaderProps {
-  cityName: string;
-}
-
-export function Header({ cityName }: HeaderProps) {
-
+export function Header({ cityName }: { cityName: string }) {
   const cityClass = cityName.toLowerCase()
+  
   return(
     <header className='header__container'>
       <nav className={`city-${cityClass}`}>
