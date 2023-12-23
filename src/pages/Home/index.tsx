6fec7418076c866d92ef6a4c19a50fe3b9dbec1d
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-23 17:44:54 -0300

    refactor: update Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,23 +4,21 @@ import { cities, City } from '../../utils/cities';
 import './style.scss';
 
 export function Home() {
-
   return (
     <div className="home__container">
       <header className='home__header'>
         <span>Weather</span>
         <p>select a city</p>
       </header>
+
       <main className='home__main'>
         <GlobeHemisphereEast size={140} weight='thin'/>
-
         <div className="cities__wrapper">
           {cities.map((item: City) => (
             <NavLink
               key={item.id}
               to={`/${item.city.toLowerCase()}`}
               className="custom-link"
-              
             >
               {item.city}
             </NavLink>
