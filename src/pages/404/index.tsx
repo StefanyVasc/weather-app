9646957e0d8584f569e6014e9eb125110200564a
StefanyVasc <stefany.vasc.sa@gmail.com>
2023-12-23 17:43:25 -0300

    refactor: update NotFound page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,9 +1,14 @@
+import { Header } from '../../components/Header';
+import './style.scss'
 
 export function NotFound() {
   return (
-    <div>
-      <h2>404 - Not Found</h2>
-      <p>Sorry, the page you are looking for might be in another castle.</p>
+    <div className="notfound__container">
+      <Header cityName={""}/>
+      <div className="notfound__wrapper">
+        <h2>404 - Not Found</h2>
+        <p>Sorry, the page you are looking for might be in another city.</p>
+      </div>
     </div>
   );
 }
